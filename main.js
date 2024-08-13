ac10454f72a68a2bd0f69c2e0e036b270e0e75dc
Fatma-Elsayed-Hussien <fatmaelsayedhussien@gmail.com>
2024-08-13 18:09:41 +0300

    practices

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function sum(a, b){
-    return a+b;
+function sum(a, b) {
+  return a + b;
 }
 
-function sendingEmailTo(name){
-    return `Hi ${name}
+function sendingEmailTo(name) {
+  return `Hi ${name}
     Thank you for joining us We are glad to have you on borad
 
     You now have Verification code this verfication code is only valid for the next 3 minutes 
@@ -12,33 +12,32 @@ function sendingEmailTo(name){
 
     Thanks
     John
-    `
+    `;
 }
 
-function isEven(num){
-    if(num % 2 === 0){
-        return true;
-    }
-    else{
-        return false;
-    }
-    // return num % 2 === 0 ? true : false;
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+  // return num % 2 === 0 ? true : false;
 }
 
-function getUserByID(id){ 
-    if(!id){
-        throw new Error("id is not defined");
-    }
-   return {
-    id: 1, 
-    name: "fatma", 
+function getUserByID(id) {
+  if (!id) {
+    throw new Error("id is not defined");
+  }
+  return {
+    id: 1,
+    name: "fatma",
     age: 24,
     address: "ismailia",
-   }
+  };
 }
-module.exports ={
-    sum,
-    sendingEmailTo,
-    isEven,
-    getUserByID
-} 
+module.exports = {
+  sum,
+  sendingEmailTo,
+  isEven,
+  getUserByID,
+};
--- main.test.js
+++ main.test.js
@@ -45,24 +45,23 @@ let fruits = ["mango", "banana", "strawberry"];
 //   {name:"ahmed", age: 22},
 //   {name:"mohamed", age: 23}
 // ]
-describe("Array", function(){
+describe("Array", function () {
   // test("toBe", function(){
   //   expect(fruits).toBe(["mango", "banana", "strawberry"]) // 0x10 === ["mango", "banana", "strawberry"]
   // });
-  test("toBe", function(){
-    expect(fruits).toEqual(["mango", "banana", "strawberry"]) 
+  test("toBe", function () {
+    expect(fruits).toEqual(["mango", "banana", "strawberry"]);
   });
-  test("toBe", function(){
-    expect(fruits).toEqual(expect.arrayContaining(["mango", "strawberry"])) 
+  test("toBe", function () {
+    expect(fruits).toEqual(expect.arrayContaining(["mango", "strawberry"]));
   });
-  test("toBe", function(){
-    expect(fruits).toContain("mango") 
+  test("toBe", function () {
+    expect(fruits).toContain("mango");
   });
   // test("toBe", function(){
-  //   expect(users).toContainEqual({name:"ahmed", age: 22}) 
+  //   expect(users).toContainEqual({name:"ahmed", age: 22})
   // });
-
-})
+});
 
 // describe("Array", function () {
 //   test("check if array contain specific item", function () {
@@ -82,81 +81,77 @@ describe("Array", function(){
 //     expect(fruits).toEqual(expect.arrayContaining(["mango", "banana"]));
 //   });
 // });
-describe("Object", function(){
-  test("obj test", function(){
+describe("Object", function () {
+  test("obj test", function () {
     // expect(getUserByID(1)).toBe({})
     expect(getUserByID(1)).toEqual({
-      id: 1, 
-      name: "fatma", 
+      id: 1,
+      name: "fatma",
       age: 24,
       address: "ismailia",
-     })
-     expect(getUserByID(1)).toEqual(
-      expect.objectContaining({id: 1, name: "fatma"})
-     )
-     expect(getUserByID(1)).toMatchObject({id: 1, name: "fatma"})
-     expect(getUserByID(1)).toHaveProperty("id", 1);
-
-  })
-  it("should throw error if id is not defined", function(){
+    });
+    expect(getUserByID(1)).toEqual(
+      expect.objectContaining({ id: 1, name: "fatma" })
+    );
+    expect(getUserByID(1)).toMatchObject({ id: 1, name: "fatma" });
+    expect(getUserByID(1)).toHaveProperty("id", 1);
+  });
+  it("should throw error if id is not defined", function () {
     // expect(() => getUserByID()).toThrow();
-    expect(function () {return getUserByID()}).toThrow();
-  })
-})
-describe("Anything", function(){
+    expect(function () {
+      return getUserByID();
+    }).toThrow();
+  });
+});
+describe("Anything", function () {
   let y; // undefined
-  let z = null; 
-  test("anything", function(){
+  let z = null;
+  test("anything", function () {
     expect("fatma").toEqual(expect.anything());
     expect(33).toEqual(expect.anything());
-    expect([1,2,3]).toEqual(expect.anything());
+    expect([1, 2, 3]).toEqual(expect.anything());
     // expect(y).toEqual(expect.anything()); //FAILL
     // expect(z).toEqual(expect.anything()); //FAILL
-  })
-})
-describe("Any", function(){
-  test("any", function(){
+  });
+});
+describe("Any", function () {
+  test("any", function () {
     expect("fatma").toEqual(expect.any(String));
     expect(33).toEqual(expect.any(Number));
     // expect(y).toEqual(expect.anything()); //FAILL
     // expect(z).toEqual(expect.anything()); //FAILL
-  })
-})
-
+  });
+});
 
 let users = [
-  {name:"ahmed", age: 22},
-  {name:"mohamed", age: 21},
-]
-users.push({name: "fatma", age: 24})
+  { name: "ahmed", age: 22 },
+  { name: "mohamed", age: 21 },
+];
+users.push({ name: "fatma", age: 24 });
 
-describe("Users List", function(){
-  test("array should have at least one object have name(string) and age(number)", function(){  
+describe("Users List", function () {
+  test("array should have at least one object have name(string) and age(number)", function () {
     expect(users).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
           name: expect.any(String),
           age: expect.any(Number),
-        })
+        }),
       ])
-    )
-  }
-  )
+    );
+  });
   // strictly check that all objects match exactly
-  test("every object should have the name (string) and age (number) in array", function(){  
-    users.forEach((user) => { 
+  test("every object should have the name (string) and age (number) in array", function () {
+    users.forEach((user) => {
       expect(user).toEqual(
         expect.objectContaining({
-        name: expect.any(String),
-        age: expect.any(Number),
-      }))
-    }
-    )
-  })
-})
-
-
-
+          name: expect.any(String),
+          age: expect.any(Number),
+        })
+      );
+    });
+  });
+});
 
 // describe("Object", function () {
 //   test("Check if user exist", function () {
@@ -212,19 +207,13 @@ describe("Users List", function(){
 //   });
 // });
 
-
 // ✨✨✨✨ ------ Task  ------ ✨✨✨✨
 // AddToCart=[
 //   {
-//     id: 
-//     name:       
+//     id:
+//     name:
 //     price:             // greater than 100 .. test case 0 or -negative
 //     quantity:          // greater than 1 & lessthan 5 .. test case 0 or negative
 //     addedtoFav: true   // Truthiness
 //   }
 // ]
-
-
-
-
-
