7a83cf3a0b22cd6ba88325d54ef305fe57ef8334
Omar <117028936+O-Abdelaziz@users.noreply.github.com>
2024-08-19 23:43:07 +0300

    Delete Tasks-Solutions/Omar_Abdelaziz directory

--- Tasks-Solutions/Omar_Abdelaziz/email.js
+++
@@ -1,9 +0,0 @@
-var emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-function vEmail(email){
-    if (typeof email !== "string") {
-        throw new Error("Please enter string");
-    }
-    return emailReg.test(email);
-  }
-
-module.exports = { vEmail };
